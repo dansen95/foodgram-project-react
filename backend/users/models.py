90917384e0489d1c1ce5fb513afcf83b5755a56d
Sen <daniil.sen@accenture.com>
2021-08-25 18:47:51 +0300

    deploy 16

--- backend/users/models.py
+++ backend/users/models.py
@@ -5,7 +5,15 @@ from django.db import models
 class CustomUser(AbstractUser):
     email = models.EmailField(
         verbose_name='email', max_length=255, unique=True)
-    REQUIRED_FIELDS = ['username', 'first_name', 'last_name', 'email']
+    username = models.CharField(max_length=150,
+                                verbose_name="username",
+                                unique=True)
+    first_name = models.CharField(max_length=150,
+                                  verbose_name="first_name")
+    last_name = models.CharField(max_length=150,
+                                 verbose_name="last_name")
+
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
     USERNAME_FIELD = 'email'
 
     def __str__(self):
