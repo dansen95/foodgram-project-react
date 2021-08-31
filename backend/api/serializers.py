2b03c3ae908abebba13aff1f06a125cff759dea7
Sen <daniil.sen@accenture.com>
2021-08-31 16:07:48 +0300

    37

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -307,7 +307,7 @@ class CreateRecipeSerializer(serializers.ModelSerializer):
 
     @transaction.atomic
     def create(self, validated_data):
-        ingredients_data = validated_data.pop('ingredients_data')
+        ingredients_data = validated_data.pop('ingredients')
         tags_data = validated_data.pop('tags')
         author = self.context.get('request').user
         recipe = Recipe.objects.create(author=author, **validated_data)
@@ -318,7 +318,7 @@ class CreateRecipeSerializer(serializers.ModelSerializer):
 
     @transaction.atomic
     def update(self, instance, validated_data):
-        ingredients_data = validated_data.pop('ingredients_data')
+        ingredients_data = validated_data.pop('ingredients')
         tags_data = validated_data.pop('tags')
         IngredientInRecipe.objects.filter(recipe=instance).delete()
         self.create_ingredients(instance, ingredients_data)
