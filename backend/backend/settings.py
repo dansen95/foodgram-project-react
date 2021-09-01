6ba1ae7545daf44389eef464582da96cfdd31c23
Sen <daniil.sen@accenture.com>
2021-09-01 23:39:53 +0300

    deploy last

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -159,7 +159,8 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.'
                                 'PageNumberPagination',
-    'PAGE_SIZE': 6
+    'PAGE_SIZE': 6,
+    'SEARCH_PARAM': 'name'
 }
 
 DJOSER = {
